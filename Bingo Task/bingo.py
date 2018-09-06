8b4ac8d00a19eaed76a0e4c121b4b86747decca7
Tom Davies <41902782+tomtdigital@users.noreply.github.com>
2018-09-06 16:04:21 +0100

    replaced all recurring functions with while loops

--- Bingo Task/bingo.py
+++ Bingo Task/bingo.py
@@ -10,16 +10,27 @@ print ('After you type a value, press ENTER to submit it.')
 #Pauses to emphasise the above statement
 time.sleep(5)
 
-#Declares function for main process
+#Main process
 def begin():
-	#Without try and except, the program would fail if the values entered by the user weren't integers.
 	try: no_of_rounds = int(input('How many numbers would you like generated for this game? '))
 	except ValueError:
-		print ('Please enter a valid number')
-		begin()
+		no_of_rounds = int(input('Please enter a valid number '))
 	#Checks if the user is happy with the amount of numbers being called in the game
 	confirm_rounds = input('Are you sure? Enter \'y\' for yes and \'n\' for no: ')
-	#If happy...
+	#Loops until valid input
+	while confirm_rounds != 'y' and confirm_rounds != 'n':
+		confirm_rounds = input('Invalid input. Enter \'y\' for yes and \'n\' for no: ')
+	
+	#Loops until user sure about the number of rounds
+	while confirm_rounds == 'n':
+		try: no_of_rounds = int(input('How many numbers would you like generated for this game? '))
+		except ValueError:
+			no_of_rounds = int(input('Please enter a valid number '))
+		confirm_rounds = input('Are you sure? Enter \'y\' for yes and \'n\' for no: ')
+		while confirm_rounds != 'y' and confirm_rounds != 'n':
+			confirm_rounds = input('Invalid input. Enter \'y\' for yes and \'n\' for no: ')
+	
+	#If happy, the above loops end, and the code continues
 	if confirm_rounds == 'y':
 		print ('Lets play Bingo!')
 		ready = input('Press ENTER to get your first number: ')
@@ -29,7 +40,7 @@ def begin():
 			next_num = random.randint(1,91)
 			#A list is created for numbers already called to go in
 			already_called = []
-			#A while loop is initiated, so long as x is less than the number of rounds inputted on line 16
+			#A while loop is initiated, so long as x is less than the number of rounds inputted on line 17
 			x = 0
 			while x < no_of_rounds:
 				#The number is reset if it has already been called; x stays the same
@@ -51,25 +62,19 @@ def begin():
 			time.sleep(2)
 			print ('Thank you for playing!')
 			
-			#A function is declared, checking if the user would like to play again. If they do, it runs the main process again.
-			def play_again():
-				again = input('Would you like to play again? Enter \'y\' for yes and \'x\' to exit: ')
-				if again == 'y':
-					begin()
-				elif again == 'x':
-					print ('Goodbye!')
-					time.sleep(2)
-					exit()
-				#In case they enter a different value
-				else: 
-					play_again()
-			
-			#Runs the function at the end of the game.
-			play_again()
-	
-	#If the user changes their mind about the amount of numbers being called out. 		
-	else :
-		begin()
 
+	
+			
 #Actually runs the game
 begin()
+#Asks if they want to play again
+again = input('Would you like to play again? Enter \'y\' for yes and \'x\' to exit: ')
+#Loops until valid input
+while again != 'y' and again != 'x':
+	again = input('Invalid input. Enter \'y\' for yes and \'x\' to exit: ')
+#The game loops until the user declares 'x'
+while again == 'y':
+	begin()
+	again = input('Would you like to play again? Enter \'y\' for yes and \'x\' to exit: ')
+if again == 'x':
+	print ('Goodbye!')
